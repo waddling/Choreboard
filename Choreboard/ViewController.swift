781ed0efa39748196069fbdf71d9fe051d338ede
jdelledonne <j.delledonne4@gmail.com>
2021-03-29 14:44:39 -0400

    basic tabbar

--- Choreboard/ViewController.swift
+++ Choreboard/ViewController.swift
@@ -13,7 +13,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
+class MyTabBarController: UITabBarController {
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /* Set deafult view ot the middle tab */
+        self.selectedIndex = 1
+        
+        /* Color Settings */
+        self.tabBar.tintColor = .white                      // Selected item color
+        self.tabBar.unselectedItemTintColor = .lightGray    // Unselected item color
+        self.tabBar.barTintColor = .blue                    // Background color
+    }
+    
+}
