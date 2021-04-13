3f96d5071cfca398927fbce62510bdf2e8ca1966
jdelledonne <j.delledonne4@gmail.com>
2021-04-13 19:17:26 -0400

    added custom cell files

---
+++ Choreboard/Views/Home/ChoreCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ChoreCollectionViewCell.swift
+//  Choreboard
+//
+//  Created by Joseph Delle Donne on 4/13/21.
+//
+
+import UIKit
+
+class ChoreCollectionViewCell: UICollectionViewCell {
+    static let identifier = "ChoreCollectionViewCell"
+}
---
+++ Choreboard/Views/Home/HouseholdMemberCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HouseholdMemberCollectionViewCell.swift
+//  Choreboard
+//
+//  Created by Joseph Delle Donne on 4/13/21.
+//
+
+import UIKit
+
+class HouseholdMemberCollectionViewCell: UICollectionViewCell {
+    static let identifier = "HouseholdMemberCollectionViewCell"
+}
