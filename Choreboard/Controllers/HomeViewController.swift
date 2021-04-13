adc76caa4ee214de793ed69c61cac19ae0cc08b9
jdelledonne <j.delledonne4@gmail.com>
2021-04-13 19:07:28 -0400

    Created home screen skeleton

--- Choreboard/Controllers/HomeViewController.swift
+++ Choreboard/Controllers/HomeViewController.swift
@@ -8,20 +8,34 @@
 import UIKit
 import RealmSwift
 
+enum HomeSectionType {
+    case chores             // 0
+    case householdMembers   // 1
+}
+
 class HomeViewController: UIViewController {
-//    let userRealm: Realm
     
+//    let userRealm: Realm
 //    init(userRealm: Realm) {
 //        self.userRealm = userRealm
-//
 //        super.init(nibName: nil, bundle: nil)
-//
-//
 //    }
-//
 //    required init?(coder: NSCoder) {
 //        fatalError("init(coder:) has not been implemented")
 //    }
+    
+    private var collectionView: UICollectionView = UICollectionView(
+        frame: .zero, collectionViewLayout: UICollectionViewCompositionalLayout { sectionIndex, _ -> NSCollectionLayoutSection? in
+            return HomeViewController.createSectionLayout(section: sectionIndex)
+        }
+    )
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.tintColor = .label
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,5 +43,157 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
         title = "Home"
         view.backgroundColor = .systemBackground
+        
+        // Order here matters! add subviews after collection views
+        configureCollectionView()
+        view.addSubview(spinner)
+        
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+    
+    private func configureCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.register(UICollectionViewCell.self,
+                                forCellWithReuseIdentifier: "cell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .systemBackground
+    }
+    
+    private static func createSectionLayout(section: Int) -> NSCollectionLayoutSection {
+        switch section {
+        case 0:
+            // item
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)
+                )
+            )
+            
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            // group, scroll horizontally or vertically here
+            // for all one group, use one group and change 'group' name in section declaration
+            // vertical group inside of a horizontal group
+            let verticalGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(360)
+                ),
+                subitem: item,
+                count: 4
+            )
+            
+            let horizontalGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(0.8),
+                    heightDimension: .absolute(360)
+                ),
+                subitem: verticalGroup,
+                count: 1
+            )
+            
+            // section
+            let section = NSCollectionLayoutSection(group: horizontalGroup)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 30)
+            section.orthogonalScrollingBehavior = .groupPaging  // .continuous (if single group)
+            return section
+            
+        case 1:
+            // item
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)
+                )
+            )
+            
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+
+            // group, scroll horizontally or vertically here
+            // for all one group, use one group and change 'group' name in section declaration
+            // vertical group inside of a horizontal group
+            let verticalGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .absolute(150),
+                    heightDimension: .absolute(300)
+                ),
+                subitem: item,
+                count: 2
+            )
+            
+            let horizontalGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .absolute(150),
+                    heightDimension: .absolute(300)
+                ),
+                subitem: verticalGroup,
+                count: 1
+            )
+            
+            // section
+            let section = NSCollectionLayoutSection(group: horizontalGroup)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0)
+            section.orthogonalScrollingBehavior = .continuous
+            return section
+            
+        default:
+            // item
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)
+                )
+            )
+            
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 10, bottom: 2, trailing: 5)
+            
+            // group
+            let group = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(90)
+                ),
+                subitem: item,
+                count: 1
+            )
+            
+            // section
+            let section = NSCollectionLayoutSection(group: group)
+            return section
+        }
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if indexPath.section == 0 {
+            cell.backgroundColor = .systemTeal
+        }
+        else if indexPath.section == 1 {
+            cell.backgroundColor = .systemGreen
+        }
+        else {
+            cell.backgroundColor = .systemGray
+        }
+        
+        return cell
+    }
+    
+    
 }
